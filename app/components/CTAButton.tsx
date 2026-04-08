fd1d2e6a4429f897a1ff319ceec18faa6f82f053
mikohan <angara99@gmail.com>
2026-04-08 11:05:36 -0700

    Button fixed

--- app/components/CTAButton.tsx
+++ app/components/CTAButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export function CTAButton() {
   return (
     <Button className="mt-4 w-xs py-8 text-2xl font-bold md:mt-12 md:w-sm">
-      <Link href="/blog/why-russians-calm">Why Crisis is Comming?</Link>
+      <Link href="/blog">Why Crisis is Comming?</Link>
     </Button>
   )
 }
