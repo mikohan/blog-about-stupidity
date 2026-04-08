6a340e66550b13d69d2d1414b043daf64946d874
mikohan <angara99@gmail.com>
2026-04-08 11:55:50 -0700

    Padding bottom

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { Divider } from "./components/Devider"
-import { Footer } from "./components/Footer"
 import { Hero } from "./components/Hero"
 import { SecondSection } from "./components/SecondSection"
 
 export default function Page() {
   return (
-    <div className="mb-16 flex flex-col justify-center gap-16 px-8 md:px-0">
+    <div className="mb-20 flex flex-col justify-center gap-16 px-8 md:px-0">
       <div className="mt-16">
         <Hero />
       </div>
